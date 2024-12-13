9841f6d80e33a1803c8067f43a31d3b7082e1eeb
Michele Gallo <michelegallo997@gmail.com>
2024-12-13 16:57:49 +0100

    React_Proyect1

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './assets/Componentes/navbar/navbar.jsx'
 import Title  from './assets/Componentes/header/Title.jsx'
+import Counter from './assets/Componentes/Counter.jsx'
 
 const root= createRoot(document.getElementById('root'))
 
@@ -10,5 +11,6 @@ root.render(
   <StrictMode>
     <Navbar/>
     <Title size={32}>Zapatos</Title>
+    <Counter></Counter>
   </StrictMode>,
 )
